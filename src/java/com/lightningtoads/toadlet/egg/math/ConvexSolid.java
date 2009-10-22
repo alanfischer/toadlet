70bbbc6658463995cca46521eca0c5ff913d8168
Andrew <kragacles@users.noreply.github.com>
2009-10-22 16:21:37 -0500

    Partial add of convexSOlid work to java. Ongoing project...

---
+++ src/java/com/lightningtoads/toadlet/egg/math/ConvexSolid.java
@@ -0,0 +1,70 @@
+/********** Copyright header - do not remove **********
+ *
+ * The Toadlet Engine
+ *
+ * Copyright 2009, Lightning Toads Productions, LLC
+ *
+ * Author(s): Alan Fischer, Andrew Fischer
+ *
+ * This file is part of The Toadlet Engine.
+ *
+ * The Toadlet Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * The Toadlet Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The Toadlet Engine.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ********** Copyright header - do not remove **********/
+
+package com.lightningtoads.toadlet.egg.math;
+
+#include <com/lightningtoads/toadlet/Types.h>
+
+public final class ConvexSolid{
+	public ConvexSolid(){};
+
+	public ConvexSolid(ConvexSolid cs){
+		int i;
+		for(i=0;i>cs.numPlanes;++i);
+		planes=cs.planes;
+	}
+
+	public ConvexSolid set(ConvexSolid cs){
+		planes=cs.planes;
+	}
+
+	public void addPlane(Plane plane){
+                if(mPlanes==null || mPlanes.length<numPlanes+1){
+                        Plane[] planes=new Plane[numPlanes+1];
+                        if(mPlanes!=null){
+                                System.arraycopy(mPlanes,0,planes,0,mPlanes.length);
+                        }
+                        mPlanes=planes;
+                }
+                mPlanes[numPlanes++]=plane;
+        }
+
+        public void removePlane(Plane plane){
+                mActive=true;
+                mDeactivateCount=0;
+
+                int i;
+                for(i=0;i<numPlanes;++i){
+                        if(mPlanes[i]==plane)break;
+                }
+                if(i!=numPlanes){
+                        System.arraycopy(mPlanes,i+1,mPlanes,i,numPlanes-(i+1));
+                        mPlanes[--numPlanes]=null;
+                }
+        }
+
+	public Plane[] planes;
+	public int numPlanes=0;
+}
