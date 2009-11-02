c759d9b479e85826e802ad094c09b838d6608573
Andrew <kragacles@users.noreply.github.com>
2009-11-02 15:46:24 -0600

    Fixed the milkshape find script

--- cmake/Modules/FindMilkshape3D.cmake
+++ cmake/Modules/FindMilkshape3D.cmake
@@ -11,11 +11,11 @@ set (MS3D_FOUND "NO")
 # Milkshape3D is win32 only
 if (WIN32)
 	set (MS_PATHS
-		"C:/Program Files/MiklShape 3D 1.8.5"
-		"C:/Program Files/MiklShape 3D 1.8.4"
-		"C:/Program Files/MiklShape 3D 1.8.3"
-		"C:/Program Files/MiklShape 3D 1.8.2"
-		"C:/Program Files/MiklShape 3D 1.8.1"
+		"C:/Program Files/MilkShape 3D 1.8.5"
+		"C:/Program Files/MilkShape 3D 1.8.4"
+		"C:/Program Files/MilkShape 3D 1.8.3"
+		"C:/Program Files/MilkShape 3D 1.8.2"
+		"C:/Program Files/MilkShape 3D 1.8.1"
 	)
 	
 	set (MS_HEADERS
@@ -23,15 +23,15 @@ if (WIN32)
 		msPlugIn.h
 	)
 
-	find_program (MS3D_EXE ms3d.exe PATHS ${MS_PATHS})
+	find_program (MS3D_EXE ms3d PATHS ${MS_PATHS})
 
-	get_filename_component (MS3D_LIB_DIR ${MS3D_EXE} PATH)
-
-	find_path (MS3D_SDK_DIR ${MS_HEADERS} PATHS ${MS3D_LIB_DIR}/msLib)
+	get_filename_component (MS3D_DIR ${MS3D_EXE} PATH)
+	
+	find_path (MS3D_SDK_DIR ${MS_HEADERS} PATHS ${MS3D_DIR}/msLib)
 
 	find_library (MS3D_SDK_LIB msModelLib PATHS ${MS3D_SDK_DIR} ${MS3D_SDK_DIR}/lib)
 
-	mark_as_advanced (MS3D_SDK_DIR MS3D_SDK_LIB MS3D_DIR)
+	mark_as_advanced (MS3D_EXE MS3D_SDK_DIR MS3D_SDK_LIB MS3D_DIR)
 
 	if (MS3D_SDK_LIB AND MS3D_SDK_DIR)
 		set (MS3D_FOUND "YES")
