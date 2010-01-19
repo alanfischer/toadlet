80f250e2e7d9499c3894c3ce6e9daf9e45750750
Alan Fischer <alan.fischer@gmail.com>
2010-01-18 19:14:16 -0600

    working on knot

---
+++ src/cpp/toadlet/knot/SimpleEventServer.h
@@ -0,0 +1,52 @@
+/********** Copyright header - do not remove **********
+ *
+ * The Toadlet Engine
+ *
+ * Copyright 2009, Lightning Toads Productions, LLC
+ *
+ * Author(s): Alan Fischer, Andrew Fischer
+ *
+ * This file is part of The Toadlet Engine.
+ *
+ * The Toadlet Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * The Toadlet Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The Toadlet Engine.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ********** Copyright header - do not remove **********/
+
+#ifndef TOADLET_KNOT_SIMPLEEVENTSERVER_H
+#define TOADLET_KNOT_SIMPLEEVENTSERVER_H
+
+#include <toadlet/knot/SimpleEventServer.h>
+
+namespace toadlet{
+namespace knot{
+
+class TOADLET_API SimpleEventServer:public EventServer{
+public:
+	TOADLET_SHARED_POINTERS(SimpleEventServer);
+
+	virtual void connected(Connection::ptr connection);
+	virtual void disconnected(Connection::ptr connection);
+	
+	virtual bool sendEvent(Event event,int clientID);
+	virtual bool broadcastEvent(Event event);
+	virtual bool receiveEvent(Event::ptr &event,int &clientID);
+
+protected:
+
+};
+
+}
+}
+
+#endif
