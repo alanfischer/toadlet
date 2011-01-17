bb363da307722468a3d1e2e4255dea026114ef9b
Alan Fischer <alan.fischer@gmail.com>
2011-01-17 16:57:03 -0600

    Added missing file and updated cmake

---
+++ src/cpp/toadlet/tadpole/ContextListener.h
@@ -0,0 +1,52 @@
+/********** Copyright header - do not remove **********
+ *
+ * The Toadlet Engine
+ *
+ * Copyright 2009, Lightning Toads Productions, LLC
+ *
+ * Author(s): Alan Fischer, Andrew Fischer
+ *
+ * This file is part of The Toadlet Engine.
+ *
+ * The Toadlet Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * The Toadlet Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The Toadlet Engine.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ********** Copyright header - do not remove **********/
+
+#ifndef TOADLET_TADPOLE_CONTEXTLISTENER_H
+#define TOADLET_TADPOLE_CONTEXTLISTENER_H
+
+namespace toadlet{
+namespace peeper{
+
+class Renderer;
+
+}
+
+namespace tadpole{
+
+class ContextListener{
+public:
+	virtual void preContextReset(peeper::Renderer *renderer)=0;
+	virtual void postContextReset(peeper::Renderer *renderer)=0;
+	
+	virtual void preContextActivate(peeper::Renderer *renderer)=0;
+	virtual void postContextActivate(peeper::Renderer *renderer)=0;
+	virtual void preContextDeactivate(peeper::Renderer *renderer)=0;
+	virtual void postContextDeactivate(peeper::Renderer *renderer)=0;
+};
+
+}
+}
+
+#endif
