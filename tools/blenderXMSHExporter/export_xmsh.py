f12395c1c00bdd5ab90f44eff7ae280b8ec7aad8
Andrew Fischer <andrew@lightningtoads.com>
2011-03-28 16:29:45 -0500

    Blender exporter handles material-less meshes much more cleanly now

--- tools/blenderXMSHExporter/export_xmsh.py
+++ tools/blenderXMSHExporter/export_xmsh.py
@@ -16,11 +16,14 @@ This script exports a toadlet XMSH file.
 
 Notes:
 
-Only sticky UV texture coordinates are supported. Per-face UVs will be ignored.
+Only selected objects will be exported.
+Only sticky UV texture coordinates are supported; per-face UVs will be ignored.
+Faces without a material will be assigned a dummy one.
+No operations or transformations are performed by this script, objects are exported exactly as is.
 
 Usage:
 
-Select the objects you wish to export and run this script fromt he "File->Export" menu.
+Select the objects you wish to export and run this script from the "File->Export" menu.
 """
 
 #********** Copyright header - do not remove **********
@@ -61,11 +64,15 @@ def write(filename):
 	for ob in objects:
 		if ob.type=='Mesh': 
 			mesh = ob.getData(mesh=True)
-			out.write('\t<Mesh>\n')
+
+			# Don't bother writing out empty meshes
+			if len(mesh.verts)==0:
+				continue;
 
 			# Write out all vertexes in the mesh at once
+			out.write('\t<Mesh>\n')
 			out.write('\t\t<Vertexes Count=\"%d\" ' % (len(mesh.verts)))
-			# TODO: add Bones, and thus skeletons
+			# TODO: add Bones, and therefore skeletons
 			out.write('Type=\"Position,Normal') 
 			if mesh.vertexUV:
 				out.write(',TexCoord')
@@ -77,63 +84,52 @@ def write(filename):
 				out.write('\n')
 			out.write('\t\t</Vertexes>\n')
 
-			# Deal with materials, if present
-			if len(mesh.materials)==0:
-				# No materials are present, so dump all faces to the same submesh with an empty material
-				# TODO: Would it be cleaner to create an empty material from the get-go, and assign all non-owned
-				# faces to that materail? Then only export out materials that actually have some faces?
+			# Generate a list of face vertex indices by material; adding a dummy material if none exists
+			xmshMaterials=mesh.materials
+			if len(xmshMaterials)==0:
+				xmshMaterials.append(Blender.Material.New(''))
+			matFaceIndicies=[[] for i in range(len(xmshMaterials))]
+			for face in mesh.faces:
+				for vert in face.verts:
+					matFaceIndicies[face.mat].append(vert.index)
+
+			# Write out one submesh per mesh material
+			for i in range(len(matFaceIndicies)):
+				submesh=matFaceIndicies[i]
+
+				# Don't bother writing out empty submeshes
+				if len(submesh)==0:
+					continue
+
 				out.write('\t\t<SubMesh>\n')
-				indicies=0
-				for face in mesh.faces:
-					indicies+=len(face.verts)
-				out.write('\t\t\t<Indexes Count=\"%d\">' % (indicies))
-				for face in mesh.faces:
-					for vert in face.verts:
-						out.write('%d '% (vert.index))
+				out.write('\t\t\t<Indexes Count=\"%d\">' % (len(submesh)))
+				for index in submesh:
+					out.write('%d ' % (index))
 				out.write('</Indexes>\n')
-				out.write('\t\t\t<Material Name="">\n')
-				out.write('\t\t\t</Material>\n')
+
+				mat=xmshMaterials[i]
+				if mat:
+					out.write('\t\t\t<Material Name=\"%s\">\n' % (mat.name))
+					
+					# TODO: Right now the mapping of blender material lightning effects to toadlet XMSH is unclear
+					out.write('\t\t\t\t<LightEffect>\n')
+					out.write('\t\t\t\t\t<Ambient>%f,%f,%f,%f</Ambient>\n' % (mat.getRGBCol()[0],mat.getRGBCol()[1],mat.getRGBCol()[2],mat.getAlpha()))
+					out.write('\t\t\t\t\t<Diffuse>%f,%f,%f,%f</Diffuse>\n' % (mat.getRGBCol()[0],mat.getRGBCol()[1],mat.getRGBCol()[2],mat.getAlpha()))
+					out.write('\t\t\t\t\t<Specular>%f,%f,%f,%f</Specular>\n' % (mat.getSpecCol()[0],mat.getSpecCol()[1],mat.getSpecCol()[2],mat.getAlpha()))
+					out.write('\t\t\t\t\t<Shininess>%f</Shininess>\n' % (mat.getSpec()))
+					out.write('\t\t\t\t\t<Emmissive>%f,%f,%f,%f</Emmissive>\n' % (mat.getEmit(),mat.getEmit(),mat.getEmit(),mat.getAlpha()))
+					out.write('\t\t\t\t</LightEffect>\n')
+
+					# TODO: Right now if a material is set to SHADELESS it is unlit, otherwise light affects it
+					if mat.getMode() & Blender.Material.Modes['SHADELESS']:
+						out.write('\t\t\t\t<Lighting>false</Lighting>\n')
+					else:
+						out.write('\t\t\t\t<Lighting>true</Lighting>\n')
+
+					# TODO: No idea with this one; how does blender determine culling?
+					out.write('\t\t\t\t<FaceCulling>back</FaceCulling>\n')
+					out.write('\t\t\t</Material>\n')
 				out.write('\t\t</SubMesh>\n')
-				
-			else:
-				# Generate list of face vertex indices by material
-				matFaceIndicies=[[] for i in range(len(mesh.materials))]
-				for face in mesh.faces:
-					for vert in face.verts:
-						matFaceIndicies[face.mat].append(vert.index)
-
-				# Write out one submesh per mesh material
-				for i in range(len(matFaceIndicies)):
-					submesh=matFaceIndicies[i]
-					out.write('\t\t<SubMesh>\n')
-					out.write('\t\t\t<Indexes Count=\"%d\">' % (len(submesh)))
-					for index in submesh:
-						out.write('%d ' % (index))
-					out.write('</Indexes>\n')
-
-					mat=mesh.materials[i]
-					if mat:
-						out.write('\t\t\t<Material Name=\"%s\">\n' % (mat.name))
-						
-						# TODO: Right now the mapping of blender material lightning effects to toadlet XMSH is unclear
-						out.write('\t\t\t\t<LightEffect>\n')
-						out.write('\t\t\t\t\t<Ambient>%f,%f,%f,%f</Ambient>\n' % (mat.getRGBCol()[0],mat.getRGBCol()[1],mat.getRGBCol()[2],mat.getAlpha()))
-						out.write('\t\t\t\t\t<Diffuse>%f,%f,%f,%f</Diffuse>\n' % (mat.getRGBCol()[0],mat.getRGBCol()[1],mat.getRGBCol()[2],mat.getAlpha()))
-						out.write('\t\t\t\t\t<Specular>%f,%f,%f,%f</Specular>\n' % (mat.getSpecCol()[0],mat.getSpecCol()[1],mat.getSpecCol()[2],mat.getAlpha()))
-						out.write('\t\t\t\t\t<Shininess>%f</Shininess>\n' % (mat.getSpec()))
-						out.write('\t\t\t\t\t<Emmissive>%f,%f,%f,%f</Emmissive>\n' % (mat.getEmit(),mat.getEmit(),mat.getEmit(),mat.getAlpha()))
-						out.write('\t\t\t\t</LightEffect>\n')
-
-						# TODO: Right now if a material is set to SHADELESS it is unlit, otherwise light affects it
-						if mat.getMode() & Blender.Material.Modes['SHADELESS']:
-							out.write('\t\t\t\t<Lighting>false</Lighting>\n')
-						else:
-							out.write('\t\t\t\t<Lighting>true</Lighting>\n')
-
-						# TODO: No idea with this one, how does blender cull?
-						out.write('\t\t\t\t<FaceCulling>back</FaceCulling>\n')
-						out.write('\t\t\t</Material>\n')
-					out.write('\t\t</SubMesh>\n')
 
 			out.write('\t</Mesh>\n')
 
