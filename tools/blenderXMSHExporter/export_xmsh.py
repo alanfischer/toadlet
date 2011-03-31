ea5ff02212541e81a880d4c7057803cc1f9d0270
Andrew Fischer <andrew@lightningtoads.com>
2011-03-31 08:38:02 -0500

    Updated blender exporter with basic skeleton support. Tests required to make sure bone matricies are correct.

--- tools/blenderXMSHExporter/export_xmsh.py
+++ tools/blenderXMSHExporter/export_xmsh.py
@@ -20,6 +20,7 @@ Only selected objects will be exported.
 Only sticky UV texture coordinates are supported; per-face UVs will be ignored.
 Faces without a material will be assigned a dummy one.
 No operations or transformations are performed by this script, objects are exported exactly as is.
+Bones with idential names are not supported, and will result in badly exported skeletons
 
 Usage:
 
@@ -55,32 +56,55 @@ import Blender
 import bpy
 
 def write(filename):
+	# Exporter specific variables
+	mBoneIndicies={}
+	mBoneCounter=0
+
 	out = open(filename, "w")
 	out.write('<XMSH Version="3">\n')
 
 	scene = bpy.data.scenes.active
 	objects = scene.objects.selected
 
+	# The XMSH format references bones by index, not by name as blender does.
+	# Find all armatures to create a name,index dictionary for all bones.
+	for ob in objects:
+		if ob.type=='Armature':
+			armature = ob.getData()
+
+			for name in armature.bones.keys():
+				if name in mBoneIndicies:
+					# TODO: Actually warn the user with a popup or something useful
+					# TODO: Perhaps this should be part of some larger check to ensure the objects being
+					# exported are suitable; then popup and warn the user if not and what the reason is
+					print 'Warning! Duplicate bone names detected, exported skeletons will be incorrect'
+				mBoneIndicies[name]=mBoneCounter
+				mBoneCounter+=1
+
+	# Export all selected meshes
 	for ob in objects:
 		if ob.type=='Mesh': 
 			mesh = ob.getData(mesh=True)
-
-			# Don't bother writing out empty meshes
 			if len(mesh.verts)==0:
 				continue;
 
 			# Write out all vertexes in the mesh at once
 			out.write('\t<Mesh>\n')
 			out.write('\t\t<Vertexes Count=\"%d\" ' % (len(mesh.verts)))
-			# TODO: add Bones, and therefore skeletons
 			out.write('Type=\"Position,Normal') 
 			if mesh.vertexUV:
 				out.write(',TexCoord')
+			if len(mBoneIndicies)>0:
+				out.write(',Bone')
 			out.write('\">\n')
 			for vert in mesh.verts:
 				out.write('\t\t\t%f,%f,%f %f,%f,%f' % (vert.co.x,vert.co.y,vert.co.z,vert.no.x,vert.no.y,vert.no.z))
 				if mesh.vertexUV:
 					out.write(' %f,%f' % (vert.uvco.x,vert.uvco.y))
+				if len(mBoneIndicies)>0:
+					bonePairs=mesh.getVertexInfluences(vert.index)
+					for bone in bonePairs:
+						out.write(' %d,%f' % (mBoneIndicies[bone[0]],bone[1]))
 				out.write('\n')
 			out.write('\t\t</Vertexes>\n')
 
@@ -96,8 +120,6 @@ def write(filename):
 			# Write out one submesh per mesh material
 			for i in range(len(matFaceIndicies)):
 				submesh=matFaceIndicies[i]
-
-				# Don't bother writing out empty submeshes
 				if len(submesh)==0:
 					continue
 
@@ -132,6 +154,40 @@ def write(filename):
 				out.write('\t\t</SubMesh>\n')
 
 			out.write('\t</Mesh>\n')
+
+	# Export all selected armatures
+	for ob in objects:
+		if ob.type=='Armature':
+			armature = ob.getData()
+
+			out.write('\t<Skeleton>\n')
+
+			for bone in armature.bones.values():
+				out.write('\t\t<Bone')
+				out.write(' Index=\"%d\"' % (mBoneIndicies[bone.name]))
+				if bone.parent:
+					out.write(' Parent=\"%d\"' % (mBoneIndicies[bone.parent.name]))
+				else:
+					out.write(' Parent=\"-1\"')
+				out.write(' Name=\"%s\"' % (bone.name))
+				out.write('>\n')
+
+				# TODO:
+				# boneMat=bone.matrix['BONESPACE'] <- This gives a 3x3 of rotations only. Not sure of it's value yet...
+				# Hints on blender bone matricies here: http://www.devmaster.net/forums/showthread.php?t=15178
+				# The XMSH format requires the root bone coordinates in worldspace; make sure we are getting this.
+				boneMat=bone.matrix['ARMATURESPACE']
+				out.write('\t\t\t<Translate>%f,%f,%f</Translate>\n' % (boneMat.translationPart().x, boneMat.translationPart().y, boneMat.translationPart().z))
+				out.write('\t\t\t<Rotate>%f,%f,%f</Rotate>\n' % (boneMat.toEuler().x, boneMat.toEuler().y, boneMat.toEuler().z))
+				out.write('\t\t\t<Scale>%f,%f,%f</Scale>\n' % (boneMat.scalePart().x, boneMat.scalePart().y, boneMat.scalePart().z))
+
+				# TODO: apparently these are depreciated. Double check to be sure.
+				#out.write('\t\t\t<WorldToBoneTranslate>%f,%f,%f</WorldToBoneTranslate>\n' % ())
+				#out.write('\t\t\t<WorldToBoneRotate>%f,%f,%f</WorldToBoneRotate>\n' % ())
+			
+				out.write('\t\t</Bone>\n')
+
+			out.write('\t<Skeleton>\n')
 
 	out.write('</XMSH>\n')
 	out.close()
