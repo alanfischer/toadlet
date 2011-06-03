f8dcfa6e102c42223fb4b5e19d4284c9df17314a
Alan Fischer <alan.fischer@gmail.com>
2011-06-03 13:24:43 -0500

    Added RandIsle iphone build

---
+++ examples/tadpole/randisle/iphone/Classes/RandIsleAppDelegate.mm
@@ -0,0 +1,43 @@
+//
+//  LogoAppDelegate.m
+//  Logo 
+//
+//  Created by Alan Fischer on 7/10/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import "LogoAppDelegate.h"
+#include "Logo.h"
+
+@implementation LogoAppDelegate
+
+@synthesize window;
+
+- (void) startApplication:(id) mainWindow{
+	if(logo!=NULL){
+		return;
+	}
+
+	logo=new Logo();
+	
+	logo->setWindow(mainWindow);
+	logo->create();
+	logo->start();
+}
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application {    
+	[application setStatusBarHidden:YES animated:NO];
+	[self startApplication:window];
+	[window makeKeyAndVisible];
+}
+
+
+- (void)dealloc {
+	logo->destroy();
+	delete logo;
+    [window release];
+    [super dealloc];
+}
+
+
+@end
