7f7a82c1c4f03219e75b70ffad8309933ad96829
Andrew Fischer <andrew@lightningtoads.com>
2011-07-26 09:46:10 -0500

    Removed compiler tests from iPhoneOS build; they will fail anyway. Removed SSE support test from iPhoneOS build as well.

--- cmake/Toolchains/iPhoneOS.cmake
+++ cmake/Toolchains/iPhoneOS.cmake
@@ -4,11 +4,18 @@ message ("Setting up iPhoneOS toolchain")
 set (TOADLET_PLATFORM_IPHONE ON CACHE BOOL "Enable the iPhone platform" FORCE)
 set (CMAKE_SYSTEM_NAME iPhoneOS)
 
-# Skip the platform compiler checks for cross compiling since they're a headache
+# Force the compilers to gcc for iOS
+include (CMakeForceCompiler)
+CMAKE_FORCE_C_COMPILER (gcc gcc)
+CMAKE_FORCE_CXX_COMPILER (g++ g++)
+
+# Skip the platform compiler checks for cross compiling
 set (CMAKE_CXX_COMPILER_WORKS TRUE)
 set (CMAKE_C_COMPILER_WORKS TRUE)
 
-# Include extra modules
+message ("toolchain file=${CMAKE_TOOLCHAIN_FILE}")
+
+# Include extra modules for the iOS platform files
 set (CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${PROJECT_SOURCE_DIR}/cmake/Modules/")
 
 message ("iPhoneOS toolchain loaded")
