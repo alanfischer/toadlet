6b8a8d5b448827172c961c9708e8e2a5a38db032
Andrew Fischer <andrew@lightningtoads.com>
2011-09-07 17:05:31 -0500

    Android build now installs libraries into armeabi/armeabi-v71/x86 subdirs just like the NDK

--- cmake/Modules/Findtoadlet.cmake
+++ cmake/Modules/Findtoadlet.cmake
@@ -26,6 +26,9 @@
 #  TOADLET_TADPOLE_LIB
 #  TOADLET_TADPOLE_HOP_LIB
 #  TOADLET_PAD_LIB
+# Android builds with ndk api level < 9 only
+#  TOADLET_PEEPER_JAR
+#  TOADLET_PAD_JAR
 # 
 # Caveats:
 # The generator will be queried for the build configurations (debug or optimized) and the proper library types assigned.
@@ -83,6 +86,42 @@ set (TOADLET_LIB_BASENAMES
 	toadlet_pad
 )
 
+# Android specifics
+if (ANDROID) 
+	if (${ANDROID_NDK_API_LEVEL} LESS 9)
+		set (LIBRARY_SEARCH_SUFFIXES ${NDK_NAME_ARCH})
+		set (TOADLET_JAR_BASENAMES jtoadlet_peeper jtoadlet_pad)
+
+		# Search for and assign the android jar files
+		foreach (TOADLET_JAR ${TOADLET_JAR_BASENAMES})
+			string (TOUPPER ${TOADLET_JAR} TOADLET_JAR_VAR)
+
+			# Only search for a library if we haven't already
+			# Otherwise the debug/release assignment settings below will cause trouble with multiple runs
+			if (NOT ${TOADLET_JAR_VAR}_FOUND)
+				# Find release and debug
+				find_file (${TOADLET_JAR_VAR}_JAR NAMES ${TOADLET_JAR}.jar PATHS ${LIBRARY_SEARCH_PATHS} PATH_SUFFIXES lib)
+				find_file (${TOADLET_JAR_VAR}_JAR_D NAMES ${TOADLET_JAR}_d.jar PATHS ${LIBRARY_SEARCH_PATHS} PATH_SUFFIXES lib)
+
+				# If only release libraries are found, assign them to the debug libraries
+				if (${TOADLET_JAR_VAR}_JAR AND NOT ${TOADLET_JAR_VAR}_JAR_D)
+					set (${TOADLET_JAR_VAR}_JAR_D ${${TOADLET_JAR_VAR}_JAR} CACHE FILEPATH "Path to a jar" FORCE)
+					set (${TOADLET_JAR_VAR}_FOUND "YES")
+				endif (${TOADLET_JAR_VAR}_JAR AND NOT ${TOADLET_JAR_VAR}_JAR_D)
+				
+				# If only debug libraries are found, assign them to the release libraries
+				if (${TOADLET_JAR_VAR}_JAR_D AND NOT ${TOADLET_JAR_VAR}_JAR)
+					set (${TOADLET_JAR_VAR}_JAR ${${TOADLET_JAR_VAR}_JAR_D} CACHE FILEPATH "Path to a jar" FORCE)
+					set (${TOADLET_JAR_VAR}_FOUND "YES")
+				endif (${TOADLET_JAR_VAR}_JAR_D AND NOT ${TOADLET_JAR_VAR}_JAR)
+				
+				set (${TOADLET_JAR_VAR}_FOUND ${${TOADLET_JAR_VAR}_FOUND} CACHE BOOL "Jar ${TOADLET_JAR} found flag" FORCE)
+				mark_as_advanced (${TOADLET_JAR_VAR}_JAR ${TOADLET_JAR_VAR}_JAR_D ${TOADLET_JAR_VAR}_FOUND)
+			endif (NOT ${TOADLET_JAR_VAR}_FOUND)
+		endforeach (TOADLET_JAR)
+	endif (${ANDROID_NDK_API_LEVEL} LESS 9)
+endif (ANDROID)
+
 # Search for and assign the toadlet libraries
 foreach (TOADLET_LIB ${TOADLET_LIB_BASENAMES})
 	string (TOUPPER ${TOADLET_LIB} TOADLET_LIB_VAR)
@@ -91,10 +130,10 @@ foreach (TOADLET_LIB ${TOADLET_LIB_BASENAMES})
 	# Otherwise the debug/release assignment settings below will cause trouble with multiple runs
 	if (NOT ${TOADLET_LIB_VAR}_FOUND)
 		# Find dynamic release, dynamic debug, static release and then static debug
-		find_library (${TOADLET_LIB_VAR}_LIB NAMES ${TOADLET_LIB} PATHS ${LIBRARY_SEARCH_PATHS})
-		find_library (${TOADLET_LIB_VAR}_LIB_D NAMES ${TOADLET_LIB}_d PATHS ${LIBRARY_SEARCH_PATHS})
-		find_library (${TOADLET_LIB_VAR}_LIB_S NAMES ${TOADLET_LIB}_s PATHS ${LIBRARY_SEARCH_PATHS})
-		find_library (${TOADLET_LIB_VAR}_LIB_SD NAMES ${TOADLET_LIB}_sd PATHS ${LIBRARY_SEARCH_PATHS})
+		find_library (${TOADLET_LIB_VAR}_LIB NAMES ${TOADLET_LIB} PATHS ${LIBRARY_SEARCH_PATHS} PATH_SUFFIXES ${LIBRARY_SEARCH_SUFFIXES})
+		find_library (${TOADLET_LIB_VAR}_LIB_D NAMES ${TOADLET_LIB}_d PATHS ${LIBRARY_SEARCH_PATHS} PATH_SUFFIXES ${LIBRARY_SEARCH_SUFFIXES})
+		find_library (${TOADLET_LIB_VAR}_LIB_S NAMES ${TOADLET_LIB}_s PATHS ${LIBRARY_SEARCH_PATHS} PATH_SUFFIXES ${LIBRARY_SEARCH_SUFFIXES})
+		find_library (${TOADLET_LIB_VAR}_LIB_SD NAMES ${TOADLET_LIB}_sd PATHS ${LIBRARY_SEARCH_PATHS} PATH_SUFFIXES ${LIBRARY_SEARCH_SUFFIXES})
 
 		# The first toadlet library we find sets the TOADLET_LIBRARY_DIR path
 		if (NOT TOADLET_LIBRARY_DIR)
@@ -143,6 +182,8 @@ foreach (TOADLET_LIB ${TOADLET_LIB_BASENAMES})
 		mark_as_advanced (${TOADLET_LIB_VAR}_LIB ${TOADLET_LIB_VAR}_LIB_D ${TOADLET_LIB_VAR}_LIB_S ${TOADLET_LIB_VAR}_LIB_SD ${TOADLET_LIB_VAR}_FOUND)
 	endif (NOT ${TOADLET_LIB_VAR}_FOUND)
 endforeach (TOADLET_LIB)
+
+
 
 # Consider toadlet found if we have the header file and a library path
 if (TOADLET_INCLUDE_DIR AND TOADLET_LIBRARY_DIR)
