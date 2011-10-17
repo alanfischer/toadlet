8201af544508579b1d26837522f50dd498551b69
Andrew Fischer <andrew@lightningtoads.com>
2011-10-17 14:43:55 -0500

    Added script to find static libraries

--- cmake/Modules/ToadletMergeStaticLibraries.cmake
+++ cmake/Modules/ToadletMergeStaticLibraries.cmake
@@ -4,29 +4,25 @@
 
 macro (TOADLET_MERGE_STATIC_LIBRARIES TARGET LIBRARIES)
 	if (TOADLET_PLATFORM_ANDROID)
-		# Android platforms are like Posix, so 
+		# Android platforms are like Posix, so merge all objects into a single archive
 		configure_file (PosixMergeStaticLibraries.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/PosixMergeStaticLibraries${TARGET}.cmake @ONLY)
 		add_custom_command (TARGET ${TARGET} POST_BUILD
 			COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/PosixMergeStaticLibraries${TARGET}.cmake
 		)
 	elseif (TOADLET_PLATFORM_WIN32)
-		# To use STATIC_LIBRARY_FLAGS on Windows you must directly format the LIBRARIES variable as a single string for the windows libtool
+		# On Windows you must directly format the LIBRARIES variable as a single string for the windows libtool
 		string (REPLACE ";" " " LIBS "${LIBRARIES}")
 		string (REPLACE "/" "\\" LIBS "${LIBS}")
 		set_property (TARGET ${TARGET} APPEND PROPERTY STATIC_LIBRARY_FLAGS "${LIBS}")
 	elseif (TOADLET_PLATFORM_OSX)
-		# OSX platforms work just as the cmake documentation would suggest
-		foreach (LIB ${LIBRARIES})
-			set (PROPS ${PROPS} STATIC_LIBRARY_FLAGS ${LIB})
-		endforeach (LIB)
-		set_target_properties (${TARGET} PROPERTIES ${PROPS})
+		# OSX is similar in that we need to clean up the library list for libtool
+		string (REPLACE ";" " " LIBS "${LIBRARIES}")
+		set_property (TARGET ${TARGET} APPEND PROPERTY STATIC_LIBRARY_FLAGS "${LIBS}")
 	elseif (TOADLET_PLATFORM_POSIX)
 		# On Posix use a configured script to merge all objects into a single archive
 		configure_file (PosixMergeStaticLibraries.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/PosixMergeStaticLibraries${TARGET}.cmake @ONLY)
 		add_custom_command (TARGET ${TARGET} POST_BUILD
 			COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/PosixMergeStaticLibraries${TARGET}.cmake
 		)
-	else (TOADLET_PLATFORM_ANDROID)
-		message (FATAL_ERROR "Toadlet platform undefined. Toadlet will not be built")
 	endif (TOADLET_PLATFORM_ANDROID)
 endmacro (TOADLET_MERGE_STATIC_LIBRARIES)
