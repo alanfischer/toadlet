e66b4bc0b4e56137a26f5da1539d21c9b46c2d0e
Andrew <kragacles@users.noreply.github.com>
2011-10-17 16:53:20 -0500

    Added OSX static library bundling, along with several other fixes.

--- cmake/Modules/LocateStaticLibrary.cmake
+++ cmake/Modules/LocateStaticLibrary.cmake
@@ -3,43 +3,43 @@
 # LIBRARIES - a list of all of the static libraries you want merged into the TARGET
 
 macro (LOCATE_STATIC_LIBRARY LIBRARY STATIC)
-	set (STATIC NOTFOUND)
+	unset (${STATIC})
 
 	if (TOADLET_PLATFORM_ANDROID)
 		get_filename_component (SUFFIX ${LIBRARY} EXT)
 		if (${SUFFIX} STREQUAL ".a")
 			# Library is already static
-			set (STATIC ${LIBRARY})
-		elseif (${SUFFIX} STREQUAL ".so"
+			set (${STATIC} ${LIBRARY})
+		elseif (${SUFFIX} STREQUAL ".so")
 			string (REPLACE ".so" ".a" LIB_S ${LIBRARY})
 			if (EXISTS ${LIB_S})
-				set (STATIC ${LIBRARY})
+				set (${STATIC} ${LIB_S})
 			endif (EXISTS ${LIB_S})
 		endif (${SUFFIX} STREQUAL ".a")
 	elseif (TOADLET_PLATFORM_WIN32)
 		# On windows we can only assume a static version exists
-		set (STATIC ${LIBRARY})
+		set (${STATIC} ${LIBRARY})
 	elseif (TOADLET_PLATFORM_OSX)
 		get_filename_component (SUFFIX ${LIBRARY} EXT)
 		if (${SUFFIX} STREQUAL ".a")
 			# Library is already static
-			set (STATIC ${LIBRARY})
-		elseif (${SUFFIX} STREQUAL ".dylib"
+			set (${STATIC} ${LIBRARY})
+		elseif (${SUFFIX} STREQUAL ".dylib")
 			string (REPLACE ".dylib" ".a" LIB_S ${LIBRARY})
 			if (EXISTS ${LIB_S})
-				set (STATIC ${LIBRARY})
+				set (${STATIC} ${LIB_S})
 			endif (EXISTS ${LIB_S})
 		endif (${SUFFIX} STREQUAL ".a")
 	elseif (TOADLET_PLATFORM_POSIX)
 		get_filename_component (SUFFIX ${LIBRARY} EXT)
 		if (${SUFFIX} STREQUAL ".a")
 			# Library is already static
-			set (STATIC ${LIBRARY})
-		elseif (${SUFFIX} STREQUAL ".so"
+			set (${STATIC} ${LIBRARY})
+		elseif (${SUFFIX} STREQUAL ".so")
 			string (REPLACE ".so" ".a" LIB_S ${LIBRARY})
 			if (EXISTS ${LIB_S})
-				set (STATIC ${LIBRARY})
+				set (${STATIC} ${LIB_S})
 			endif (EXISTS ${LIB_S})
 		endif (${SUFFIX} STREQUAL ".a")
 	endif (TOADLET_PLATFORM_ANDROID)
-endmacro (TOADLET_FIND_STATIC_LIBRARY)
+endmacro (LOCATE_STATIC_LIBRARY)
