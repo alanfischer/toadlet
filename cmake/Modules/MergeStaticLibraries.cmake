bf9741d8d88ab1f7ecdaa070ef1b628e04b68e53
Andrew Fischer <andrew@lightningtoads.com>
2011-10-17 17:41:31 -0500

    Added in static library bundling that works on posix platforms.

--- cmake/Modules/MergeStaticLibraries.cmake
+++ cmake/Modules/MergeStaticLibraries.cmake
@@ -5,9 +5,14 @@
 macro (MERGE_STATIC_LIBRARIES TARGET LIBRARIES)
 	if (TOADLET_PLATFORM_ANDROID)
 		# Android platforms are like Posix, so merge all objects into a single archive
-		configure_file (PosixMergeStaticLibraries.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/PosixMergeStaticLibraries${TARGET}.cmake @ONLY)
+		set (LIBRARIES ${LIBRARIES})
+		get_target_property (TARGETLOC ${TARGET} LOCATION)
+		configure_file (
+			${PROJECT_SOURCE_DIR}/cmake/Modules/PosixMergeStaticLibraries.cmake.in 
+			${CMAKE_CURRENT_BINARY_DIR}/PosixMergeStaticLibraries${TARGET}.cmake @ONLY
+		)
 		add_custom_command (TARGET ${TARGET} POST_BUILD
-			COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/PosixMergeStaticLibraries${TARGET}.cmake
+			COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/PosixMergeStaticLibraries-${TARGET}.cmake
 		)
 	elseif (TOADLET_PLATFORM_WIN32)
 		# On Windows you must directly format the LIBRARIES variable as a single string for the windows libtool
@@ -20,9 +25,14 @@ macro (MERGE_STATIC_LIBRARIES TARGET LIBRARIES)
 		set_property (TARGET ${TARGET} APPEND PROPERTY STATIC_LIBRARY_FLAGS "${LIBS}")
 	elseif (TOADLET_PLATFORM_POSIX)
 		# On Posix use a configured script to merge all objects into a single archive
-		configure_file (PosixMergeStaticLibraries.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/PosixMergeStaticLibraries${TARGET}.cmake @ONLY)
+		set (LIBRARIES ${LIBRARIES})
+		get_target_property (TARGETLOC ${TARGET} LOCATION)
+		configure_file (
+			${PROJECT_SOURCE_DIR}/cmake/Modules/PosixMergeStaticLibraries.cmake.in 
+			${CMAKE_CURRENT_BINARY_DIR}/PosixMergeStaticLibraries-${TARGET}.cmake @ONLY
+		)
 		add_custom_command (TARGET ${TARGET} POST_BUILD
-			COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/PosixMergeStaticLibraries${TARGET}.cmake
+			COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/PosixMergeStaticLibraries-${TARGET}.cmake
 		)
 	endif (TOADLET_PLATFORM_ANDROID)
 endmacro (MERGE_STATIC_LIBRARIES)
