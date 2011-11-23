f3fdfaa7c2738b6ca64d8564056a2d86c1f4fe5f
siralanf <siralanf@redslime>
2011-11-23 10:53:46 -0600

    Cleaned up FFmpeg stream, Added Video sample.

--- src/cpp/toadlet/tadpole/handler/FFmpegStreamContext.cpp
+++ src/cpp/toadlet/tadpole/handler/FFmpegStreamContext.cpp
@@ -4,17 +4,17 @@
 using namespace toadlet::egg;
 using namespace toadlet::egg::io;
 
-static int toadlet_read_packet(void *opaque,uint8_t *buf,int buf_size){
+int toadlet_read_packet(void *opaque,uint8_t *buf,int buf_size){
 	Stream *stream=(Stream*)opaque;
 	return stream->read(buf,buf_size);
 }
 
-static int toadlet_write_packet(void *opaque,uint8_t *buf,int buf_size){
+int toadlet_write_packet(void *opaque,uint8_t *buf,int buf_size){
 	Stream *stream=(Stream*)opaque;
 	return stream->write(buf,buf_size);
 }
 
-static int64_t toadlet_seek(void *opaque,int64_t offset,int whence){
+int64_t toadlet_seek(void *opaque,int64_t offset,int whence){
 	Stream *stream=(Stream*)opaque;
 
 	bool result=false;
@@ -34,12 +34,4 @@ static int64_t toadlet_seek(void *opaque,int64_t offset,int whence){
 	}
 
 	return result?0:-1;
-}
-
-int toadlet_ffmpeg_stream_context(ByteIOContext *context,Stream *stream,unsigned char *buffer,int size){
-	int result=init_put_byte(
-		context,buffer,size,0,stream,
-		toadlet_read_packet,toadlet_write_packet,toadlet_seek
-	);
-	return result;
 }
