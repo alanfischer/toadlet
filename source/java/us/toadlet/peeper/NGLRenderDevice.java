e4bdc53bd5c8c77038678924bc1ef99e3732e292
Alan Fischer <alan.fischer@gmail.com>
2012-01-20 14:36:25 -0600

    Added missing file

---
+++ source/java/us/toadlet/peeper/NGLRenderDevice.java
@@ -0,0 +1,41 @@
+/********** Copyright header - do not remove **********
+ *
+ * The Toadlet Engine
+ *
+ * Copyright 2009, Lightning Toads Productions, LLC
+ *
+ * Author(s): Alan Fischer, Andrew Fischer
+ *
+ * This file is part of The Toadlet Engine.
+ *
+ * The Toadlet Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * The Toadlet Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The Toadlet Engine.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ********** Copyright header - do not remove **********/
+
+package us.toadlet.peeper;
+
+public class NGLRenderDevice implements RenderDevice{
+	public NGLRenderDevice(){makeRenderDevice();}
+
+	public native boolean create(RenderTarget target,int[] options);
+	public native void destroy();
+
+	public native void swap();
+	public native void beginScene();
+	public native void endScene();	
+	
+	private native void makeRenderDevice();
+	
+	protected int mNativeHandle;
+}
