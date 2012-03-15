47f9f67773e92fae3d096d683f54afbdfc196c61
Alan Fischer <Alan Fischer@localhost>
2012-03-14 20:44:35 -0500

    Cleaned up python bindings, added mouse dragging in the python example

--- examples/tadpole/python/example.py
+++ examples/tadpole/python/example.py
@@ -14,7 +14,7 @@ scene=Scene(engine)
 
 camera=CameraNode()
 camera.create(scene)
-camera.setLookAt((0,-50,0,0),(0,0,0,0),(0,0,1,0))
+camera.setLookAt((0,-50,0),(0,0,0),(0,0,1))
 scene.getRoot().attach(camera)
 
 mesh=MeshNode()
@@ -22,13 +22,25 @@ mesh.create(scene)
 mesh.setMesh("c:\\users\\siralanf\\toadlet\\examples\\tadpole\\python\\lt.tmsh")
 scene.getRoot().attach(mesh)
 
-lastTime=time()
-window.run=True
+mesh.totalRotate=0.0
+
 def close():
 	window.run=False
 	window.destroy()
 window.protocol("WM_DELETE_WINDOW",close)
 
+def down(event):
+	window.lastx=event.x
+window.bind("<Button-1>",down)
+
+def drag(event):
+	mesh.totalRotate+=(event.x-window.lastx)/100.0
+	mesh.setRotate((0,0,1),mesh.totalRotate)
+	window.lastx=event.x;
+window.bind("<B1-Motion>",drag)
+
+lastTime=time()
+window.run=True
 while window.run:
 	newTime=time();
 	dt=(newTime-lastTime)*1000;
