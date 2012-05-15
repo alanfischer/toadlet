a9c5c9ebbe9592cf9218b31a3ecb1d37b3640db8
Andrew <kragacles@users.noreply.github.com>
2012-05-15 14:25:30 -0500

    Fixed swig includes

--- cmake/Modules/SwigWrapFiles.cmake
+++ cmake/Modules/SwigWrapFiles.cmake
@@ -1,16 +1,22 @@
 # This simple macro uses swig to wrap interface files
 # OUTPUT - the output .cpp file 
 # FILES - a list of all of the interface files you want wrapped into the output .cpp file
+# 
+# Required variables: 
+#   SWIG_EXECUTABLE = points to the location of the swig exe
+#   SWIG_LANGUAGE = the output language (java/objc/etc)
+#
+# Optional:
+#   SWIG_INCLUDES = additional include paths for swig
 #
 # Example use:
 #   SWIG_WRAP_FILES(my_wrap.cpp "${MY_FILES}")
 #
 # NOTE: When you call this script, make sure you quote the argument to FILES if it is a list!
-# NOTE: Both SWIG_EXECUTABLE and SWIG_LANGUAGE must be set to use this macro
 
 macro (SWIG_WRAP_FILES OUTPUT FILES)
 	add_custom_command (OUTPUT ${OUTPUT}
-		COMMAND ${SWIG_EXECUTABLE} -${SWIG_LANGUAGE} -c++ -outcurrentdir -o ${OUTPUT} ${FILES}
+		COMMAND ${SWIG_EXECUTABLE} -${SWIG_LANGUAGE} -I${SWIG_INCLUDES} -c++ -outcurrentdir -o ${OUTPUT} ${FILES}
 	)
 endmacro (SWIG_WRAP_FILES)
 
