8dbf6329a3e1e22df8eed08ae8b8063ba0aab69a
Andrew Fischer <andrew@ltengsoft.com>
2012-05-15 08:12:57 -0500

    First fully swig compatible commit

---
+++ cmake/Modules/SwigWrapFiles.cmake
@@ -0,0 +1,16 @@
+# This simple macro uses swig to wrap interface files
+# OUTPUT - the output .cpp file 
+# FILES - a list of all of the interface files you want wrapped into the output .cpp file
+#
+# Example use:
+#   SWIG_WRAP_FILES(my_wrap.cpp "${MY_FILES}")
+#
+# NOTE: When you call this script, make sure you quote the argument to FILES if it is a list!
+# NOTE: Both SWIG_EXECUTABLE and SWIG_LANGUAGE must be set to use this macro
+
+macro (SWIG_WRAP_FILES OUTPUT SWIG LANGUAGE FILES)
+	add_custom_command (OUTPUT ${OUTPUT}
+		COMMAND ${SWIG_EXECUTABLE} -${SWIG_LANGUAGE} -c++ -o ${OUTPUT} ${FILES}
+	)
+endmacro (SWIG_WRAP_FILES)
+
