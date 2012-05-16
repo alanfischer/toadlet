6f6accc8309a7084846fadeb76deae391908c4f8
Andrew Fischer <andrew@ltengsoft.com>
2012-05-16 09:06:11 -0500

    Updated swig wrapper macro to include setup and much cleaner use.

--- cmake/Modules/SwigWrapFiles.cmake
+++ cmake/Modules/SwigWrapFiles.cmake
@@ -1,24 +1,54 @@
-# This simple macro uses swig to wrap interface files
-# OUTPUT - the output .cpp file 
-# PACKAGE - package name you wish to output
-# FILES - a list of all of the interface files you want wrapped into the output .cpp file
-# 
-# Required variables: 
-#   SWIG_EXECUTABLE = points to the location of the swig exe
-#   SWIG_LANGUAGE = the output language (java/objc/etc)
-#   SWIG_PACKAGE_PREFIX = the package prefix (eg. us.toadlet. for java, none for objc)
-#
-# Optional:
-#   SWIG_INCLUDES = additional include paths for swig
-#
-# Example use:
-#   SWIG_WRAP_FILES(my_wrap.cpp com.my.package "${MY_FILES}")
-#
-# NOTE: When you call this script, make sure you quote the argument to FILES if it is a list!
+# This file lists 2 macros for generating swig wrapper files
 
-macro (SWIG_WRAP_FILES OUTPUT PACKAGE FILES)
+# swig_wrap_setup(LANGUAGE PREFIX INCLUDES)
+#  NOTE: This must be called at least once before using the swig_wrap_files() macro.
+#  
+#  Arguments:
+#   LANGUAGE - language of the bindings being generated (ex: java/objc)
+#   PREFIX - a package prefix (ex: my.package.is.) NOTE: set to NO if empty
+#   INCLUDES - a list of include directories for the bindings
+#
+#  Examples:
+#   SWIG_WRAP_SETUP(java com.my. /path/to/binding/includes)
+#   SWIG_WRAP_SETUP(objc NO /path/to/binding/includes)
+
+macro (SWIG_WRAP_SETUP LANGUAGE PREFIX INCLUDES)
+	if (NOT SWIG_EXECUTABLE)
+		message (FATAL_ERROR "The SWIG_EXECUTABLE is not set. Did using find_package(SWIG) fail?")
+	endif (NOT SWIG_EXECUTABLE)
+	
+	set (SWIG_WRAP_LANGUAGE ${LANGUAGE} CACHE INTERNAL "Swig wrap language" FORCE)
+	set (SWIG_WRAP_INCLUDES ${INCLUDES} CACHE INTERNAL "Swig wrap include path" FORCE)
+	
+	# The prefix may be empty, and we must redefine prefix for this comparison to work properly
+	set (PREFIX ${PREFIX})
+	if (PREFIX)
+		set (SWIG_WRAP_PREFIX ${PREFIX} CACHE INTERNAL "Swig wrap package prefix" FORCE)
+	endif (PREFIX)
+	
+	set (SWIG_WRAP_FILES_SETUP ON CACHE INTERNAL "Swig wrap files is setup" FORCE)
+	message (STATUS "SWIG_WRAP_FILES() has been setup")
+endmacro (SWIG_WRAP_SETUP)
+
+
+# swig_wrap_files(OUTPUT PACKAGE INTERFACE)
+#  This uses swig to wrap interface file to an output.
+#  NOTE: swig_wrap_setup() MUST be called before this macro may be used.
+#
+#  Arguments:
+#   OUTPUT - the output file name
+#   PACKAGE - package name you wish to output
+#   INTERFACE - the interface file you are using
+#
+#  Example:
+#   SWIG_WRAP_FILES(my_wrap.cpp packagename /path/to/my/interface.i)
+
+macro (SWIG_WRAP_FILES OUTPUT PACKAGE INTERFACE)
+	if (NOT SWIG_WRAP_FILES_SETUP)
+		message (FATAL_ERROR "The SWIG_WRAP_SETUP() macro has not been called. You must setup before using SWIG_WRAP_FILES().")
+	endif (NOT SWIG_WRAP_FILES_SETUP)
+	
 	add_custom_command (OUTPUT ${OUTPUT}
-		COMMAND ${SWIG_EXECUTABLE} -${SWIG_LANGUAGE} -I${SWIG_INCLUDES} -package ${SWIG_PACKAGE_PREFIX}${PACKAGE} -c++ -outcurrentdir -o ${OUTPUT} ${FILES}
+		COMMAND ${SWIG_EXECUTABLE} -${SWIG_WRAP_LANGUAGE} -I${SWIG_WRAP_INCLUDES} -package ${SWIG_WRAP_PREFIX}${PACKAGE} -c++ -outcurrentdir -o ${OUTPUT} ${INTERFACE}
 	)
 endmacro (SWIG_WRAP_FILES)
-
