3ef9ed6cc147e643e45c0438bf4a41e6f3ae1d1b
Alan Fischer <alan.fischer@gmail.com>
2013-08-19 08:37:32 -0500

    Fixed cloned material compilation

--- examples/randisle/src/RandIsle.cpp
+++ examples/randisle/src/RandIsle.cpp
@@ -715,9 +715,6 @@ void RandIsle::keyPressed(int key){
 		Profile::getInstance()->outputTimings();
 		Profile::getInstance()->clearTimings();
 	}
-	else if(key=='g'){
-		((BaseApplication*)mApp)->changeRenderDevicePlugin("gl");
-	}
 }
 
 void RandIsle::keyReleased(int key){
