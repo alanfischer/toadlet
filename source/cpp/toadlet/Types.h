b661fc0db8e12229910a906a98883f9eafac6ec4
Alan Fischer <alan.fischer@gmail.com>
2014-01-20 17:22:22 -0600

    clarifying types

--- source/cpp/toadlet/Types.h
+++ source/cpp/toadlet/Types.h
@@ -243,8 +243,8 @@ typedef signed char int8;
 typedef unsigned char uint8;
 typedef uint8 tbyte;
 
-typedef signed short int16;
-typedef unsigned short uint16;
+typedef signed short int int16;
+typedef unsigned short int uint16;
 
 typedef signed int int32;
 typedef unsigned int uint32;
@@ -255,8 +255,8 @@ typedef unsigned int uint32;
 	#define TOADLET_MAKE_INT64(x) x
 	#define TOADLET_MAKE_UINT64(x) x
 #else
-	typedef signed long long int64;
-	typedef unsigned long long uint64;
+	typedef signed long long int int64;
+	typedef unsigned long long int uint64;
 	#define TOADLET_MAKE_INT64(x) x##LL
 	#define TOADLET_MAKE_UINT64(x) x##ULL
 #endif
