6cac3f616e692ee82870c6f26826f6e8431de269
siralanf <alan@lightningtoads.com>
2014-07-27 10:47:08 -0500

    Added resourceTransformer, fixed bmp saver, added string splitting

---
+++ experimental/resourceTransformer/ResourceTransformer.h
@@ -0,0 +1,29 @@
+#ifndef RESOURCETRANSFORMER_H
+#define RESOURCETRANSFORMER_H
+
+#include <toadlet/toadlet.h>
+#include "mongoose.h"
+
+class ResourceTransformer:public Object,public ResourceRequest{
+public:
+	TOADLET_IOBJECT(ResourceTransformer);
+
+	ResourceTransformer();
+	virtual ~ResourceTransformer();
+
+	void update(int dt);
+
+	int handleRequest(struct mg_connection *conn, enum mg_event ev);
+
+	void resourceReady(Resource *resource){}
+	void resourceException(const Exception &ex){}
+	void resourceProgress(float progress){}
+
+	mg_server *server;
+	Engine::ptr engine;
+	MemoryStream::ptr stream;
+	Map<String,ResourceManager::ptr> managers;
+	Map<String,String> mimetypes;
+};
+
+#endif
