e54cb11f7f17d430114a863c576b3e636a6cdc87
Alan Fischer <alan.fischer@gmail.com>
2014-08-20 10:34:54 -0500

    Fixed DOMArchive creation on emscripten

--- source/js/DOMArchive.js
+++ source/js/DOMArchive.js
@@ -1,76 +1,88 @@
 mergeInto(LibraryManager.library, {
-	DOMArchive__postset: 
-               'Module["DOMArchive"] = _DOMArchive;',
+	$DOMArchive__postset: 'Module["DOMArchive"] = _DOMArchive;',
+	$DOMArchive: {},
 
-	DOMArchive:function(engine){
-		var toadlet=Module;
+	DOMArchive__deps: ['$DOMArchive'],
+	DOMArchive: function(){return Module.Archive.extend('DOMArchive',{
+		__construct : function(engine) {
+			this.__parent.__construct.call(this);
+			this.engine=engine;
+		},
+
+		__destruct : function() {
+			this.__parent.__destruct.call(this);
+		},
 		
-		return toadlet['Archive']['implement']({
-			engine : engine,
+		openStream : function(name,request){
+			var toadlet=Module;
 			
-			'openStream' : function(name,request){
-				var toadlet=Module;
-				var xmlHttp=new XMLHttpRequest();
-				xmlHttp.overrideMimeType('text\/plain; charset=x-user-defined');
-				xmlHttp.open("GET",name);
-				xmlHttp.onreadystatechange=function(){
-					if(xmlHttp.readyState==4){
-						if(xmlHttp.status==200){
-							request['stringStreamReady'](xmlHttp.responseText);
-						}
-						else{
-							request['streamException'](new toadlet['Exception']("status=="+xmlHttp.status));
-						}
-						request['delete']();
-					}
-				}.bind(this);
-				xmlHttp.ontimeout=function(){
-					request['streamException'](new toadlet['Exception']("ontimeout"));
-					request['delete']();
-				}.bind(this);
-				xmlHttp.send();
-				return true;
-			},
+			request=request['clone']();
 			
-			'openResource' : function(name,request){
-				var toadlet=Module;
-				var image=new Image();
-				image.crossOrigin = toadlet.crossOrigin || "anonymous";
-				image.onload=function(){
-					// TODO: Can we cleanly get the texture handle from emscripten?
-					var gl=toadlet.ctx;
-					var target,handle;
-					
-					var textureManager=this.engine['getTextureManager']();
-					var textureFormat=new toadlet['TextureFormat'](toadlet['Dimension']['Dimension_D2'].value,toadlet['Format']['Format_RGBA_8'].value,image.naturalWidth,image.naturalHeight,1,0);
-
-					var oldBindTexture=gl.bindTexture.bind(gl);
-					gl.bindTexture=function(gltarget,glhandle){ target=gltarget; handle=glhandle; oldBindTexture(gltarget,glhandle); };
-					var texture=textureManager['createTexture'](textureFormat);
-					gl.bindTexture=oldBindTexture;
-
-					if(texture==null){
-						request.resourceException(new toadlet['Exception']("texture==null"));
+			var xmlHttp=new XMLHttpRequest();
+			xmlHttp.overrideMimeType('text\/plain; charset=x-user-defined');
+			xmlHttp.open("GET",name);
+			xmlHttp.onreadystatechange=function(){
+				if(xmlHttp.readyState==4){
+					if(xmlHttp.status==200){
+						request['stringStreamReady'](xmlHttp.responseText);
 					}
 					else{
-						gl.bindTexture(target,handle);
-						gl.texImage2D(target,0,gl.RGBA,gl.RGBA,gl.UNSIGNED_BYTE,image);
-						gl.generateMipmap(target);
-						gl.bindTexture(target,null);
-						textureManager['manage'](texture,name);
-						request['resourceReady'](texture);
-						texture['delete']();
+						request['streamException'](new toadlet['Exception']("status=="+xmlHttp.status));
 					}
-					textureFormat['delete']();
 					request['delete']();
-				}.bind(this);
-				image.onerror=function(){
-					request['resourceException'](new toadlet['Exception']("onerror"));
-					request['delete']();
-				}.bind(this);
-				image.src=name;
-				return true;
-			}
-		});
-	}
+				}
+			}.bind(this);
+			xmlHttp.ontimeout=function(){
+				request['streamException'](new toadlet['Exception']("ontimeout"));
+				request['delete']();
+			}.bind(this);
+			xmlHttp.send();
+			
+			return true;
+		},
+		
+		openResource : function(name,request){
+			var toadlet=Module;
+
+			request=request['clone']();
+
+			var image=new Image();
+			image.crossOrigin = toadlet.crossOrigin || "anonymous";
+			image.onload=function(){
+				// TODO: Can we cleanly get the texture handle from emscripten?
+				var gl=toadlet.ctx;
+				var target,handle;
+				
+				var textureManager=this.engine['getTextureManager']();
+				var textureFormat=new toadlet['TextureFormat'](toadlet['Dimension']['Dimension_D2'].value,toadlet['Format']['Format_RGBA_8'].value,image.naturalWidth,image.naturalHeight,1,0);
+
+				var oldBindTexture=gl.bindTexture.bind(gl);
+				gl.bindTexture=function(gltarget,glhandle){ target=gltarget; handle=glhandle; oldBindTexture(gltarget,glhandle); };
+				var texture=textureManager['createTexture'](textureFormat);
+				gl.bindTexture=oldBindTexture;
+
+				if(texture==null){
+					request['resourceException'](new toadlet['Exception']("texture==null"));
+				}
+				else{
+					gl.bindTexture(target,handle);
+					gl.texImage2D(target,0,gl.RGBA,gl.RGBA,gl.UNSIGNED_BYTE,image);
+					gl.generateMipmap(target);
+					gl.bindTexture(target,null);
+					textureManager['manage'](texture,name);
+					request['resourceReady'](texture);
+					texture['delete']();
+				}
+				textureFormat['delete']();
+				request['delete']();
+			}.bind(this);
+			image.onerror=function(){
+				request['resourceException'](new toadlet['Exception']("onerror"));
+				request['delete']();
+			}.bind(this);
+			image.src=name;
+
+			return true;
+		}
+	})}
 });
